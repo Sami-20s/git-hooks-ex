9c94ce71e6517dc81f058c3a4f7d6c2c73d087b0
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-05 16:10:19 +0300

    test the readme

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ## M4 Version
 
-1. Version: 1.10.4
-2. Version: 1.10.4
-3. Version: 1.10.5
-4. Version: 1.10.6
-5. Version: 1.10.7 (Commit: 63c4a6a)
-6. Version: 1.10.8 (Commit: 7a1a02a)
-7. Version: 1.10.9 (Commit: 3e8e330)
-8. Version: 1.10.10 (Commit: 3e5c665)
-2. Version: 1.10.11 (Commit: 1cefbdf)
+Version: 1.10.4
+Version: 1.10.4
+Version: 1.10.5
+Version: 1.10.6
+Version: 1.10.7 (Commit: 63c4a6a)
+Version: 1.10.8 (Commit: 7a1a02a)
+Version: 1.10.9 (Commit: 3e8e330)
+Version: 1.10.10 (Commit: 3e5c665)
+Version: 1.10.11 (Commit: 1cefbdf)
+
--- update_version.sh
+++ update_version.sh
@@ -9,15 +9,6 @@ new_version=$(echo $current_version | awk -F. '{print $1"."$2"."$3+1}')
 # Get the commit hash
 commit_hash=$(git rev-parse --short HEAD)
 
-# Get the index number
-index_number=$(awk '/^Version: [0-9]+\.[0-9]+\.[0-9]+/{print $1}' readme.md | wc -l)
-
-# Increment the index number by 1
-index_number=$((index_number + 2))
-
-# Create a new line with the index number, incremented version, and commit hash
-new_line="$index_number. Version: $new_version (Commit: $commit_hash)"
-
 # Add a new line with the index number, incremented version, and commit hash at the end of the file
 echo "$new_line" >> readme.md
 
