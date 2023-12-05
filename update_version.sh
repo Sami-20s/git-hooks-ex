378bae6bf0f5c8016fce6d6868cb342ec1e72b64
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-05 15:33:00 +0300

    vbxsx

--- update_version.sh
+++ update_version.sh
@@ -1,1 +1,1 @@
-(Get-Content -Path "Path\To\Your\readme.md") -replace 'Version: (\d+)', {param($match) "Version: $($match.Groups[1].Value + 1)"} | Set-Content -Path "./readme.md"
+(Get-Content -Path "./readme.md") -replace 'Version: (\d+)', {param($match) "Version: $($match.Groups[1].Value + 1)"} | Set-Content -Path "./readme.md"a
