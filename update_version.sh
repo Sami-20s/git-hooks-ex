653520fe0e615bc2a60dc78a4de27fe3b35696aa
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-05 16:28:20 +0300

    wajih is gays

--- update_version.sh
+++ update_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Read the current version from the readme.md file
-current_version=$(grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+' readme.md | tail -n 1 | awk '{print $2}')
+current_version=$(grep -Eo 'v [0-9]+\.[0-9]+\.[0-9]+' readme.md | tail -n 1 | awk '{print $2}')
 
 # Increment the last digit of the version by 1
 new_version=$(echo $current_version | awk -F. '{print $1"."$2"."$3+1}')
@@ -10,7 +10,7 @@ new_version=$(echo $current_version | awk -F. '{print $1"."$2"."$3+1}')
 commit_hash=$(git rev-parse --short HEAD)
 
 # Create a new line with the incremented version and commit hash
-new_line="| v$new_version | $commit_hash |"
+new_line="| v $new_version | $commit_hash |"
 
 # Add a new line with the incremented version and commit hash at the end of the file
 echo "$new_line" >> readme.md
