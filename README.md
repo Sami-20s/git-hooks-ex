3e5c665dc79862b4924d01843968d13da7d3d720
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-05 16:05:40 +0300

    test the readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Version: 1.10.3
 Version: 1.10.4
 Version: 1.10.4
 Version: 1.10.5
 Version: 1.10.6
 Version: 1.10.7 (Commit: 63c4a6a)
 Version: 1.10.8 (Commit: 7a1a02a)
+7. Version: 1.10.9 (Commit: 3e8e330)
--- update_version.sh
+++ update_version.sh
@@ -9,10 +9,16 @@ new_version=$(echo $current_version | awk -F. '{print $1"."$2"."$3+1}')
 # Get the commit hash
 commit_hash=$(git rev-parse --short HEAD)
 
-# Create a new line with the incremented version and commit hash
-new_line="Version: $new_version (Commit: $commit_hash)"
+# Get the index number
+index_number=$(awk '/^Version: [0-9]+\.[0-9]+\.[0-9]+/{print $1}' readme.md | wc -l)
 
-# Add a new line with the incremented version and commit hash at the end of the file
+# Increment the index number by 1
+index_number=$((index_number + 1))
+
+# Create a new line with the index number, incremented version, and commit hash
+new_line="$index_number. Version: $new_version (Commit: $commit_hash)"
+
+# Add a new line with the index number, incremented version, and commit hash at the end of the file
 echo "$new_line" >> readme.md
 
-echo "Version updated to $new_version with Commit: $commit_hash"
+echo "Version updated to $new_version with Commit: $commit_hash at index $index_number"
