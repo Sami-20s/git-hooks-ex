aa5d1b2f6b03f61a73d76eca69cb1bed4e1fe6c8
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-05 17:34:55 +0300

    fofysxd

--- update_version.sh
+++ update_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Read the current version from the readme.md file
-current_version=$(grep -Eo 'v [0-9]+\.[0-9]+\.[0-9]+' readme.md | awk '{print $2}')
+current_version=$(grep -Eo 'Version: [0-9]+\.[0-9]+\.[0-9]+' readme.md | tail -n 1 | awk '{print $2}')
 
 # Increment the last digit of the version by 1
 new_version=$(echo $current_version | awk -F. '{print $1"."$2"."$3+1}')
