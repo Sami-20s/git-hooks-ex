34e46fb2f06b17f118fb86e14956271103b87a7b
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-05 15:50:26 +0300

    vbxsx

--- update_version.sh
+++ update_version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Read the current version from the readme.md file
-current_version=$(grep -Eo 'Version: [0-9]+' readme.md | awk '{print $2}')
+current_version=$(grep -Eo 'Version: [0-9]+\.[0-9]+\.[0-9]+' readme.md | awk '{print $2}')
 
-# Increment the version by 1
-new_version=$((current_version + 1))
+# Increment the last digit of the version by 1
+new_version=$(echo $current_version | awk -F. '{print $1"."$2"."$3+1}')
 
 # Update the readme.md file with the new version
 sed -i "s/Version: $current_version/Version: $new_version/" readme.md
