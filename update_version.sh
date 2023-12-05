1a32abcb25884a40614876376bbe0af9cef85281
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-05 15:32:04 +0300

    vbxsx

--- update_version.sh
+++ update_version.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-# Assuming your version is stored in a file named "version.txt"
-version=$(cat version.txt)
-# readMe=$(cat README.md)
-# echo $readMe
-# Update the version in README.md
-echo "Version:1.2.3" > README.md
-readMe=$(cat README.md)
-echo $readMe
+(Get-Content -Path "Path\To\Your\readme.md") -replace 'Version: (\d+)', {param($match) "Version: $($match.Groups[1].Value + 1)"} | Set-Content -Path "./readme.md"
