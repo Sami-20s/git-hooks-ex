a94c0ebcbb0ed4e13059bedf5e1b85a52827605a
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-05 15:57:46 +0300

    zxc

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Version: 1.10.3
 Version: 1.10.4
 Version: 1.10.4
+Version: 1.10.5
--- update_version.sh
+++ update_version.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Read the current version from the readme.md file
-current_version=$(grep -Eo 'Version: [0-9]+\.[0-9]+\.[0-9]+' readme.md | awk '{print $2}')
+current_version=$(grep -Eo 'Version: [0-9]+\.[0-9]+\.[0-9]+' readme.md | tail -n 1 | awk '{print $2}')
 
 # Increment the last digit of the version by 1
 new_version=$(echo $current_version | awk -F. '{print $1"."$2"."$3+1}')
 
-# Add a new line with the incremented version after the second line
-sed -i "2 a Version: $new_version" readme.md
+# Add a new line with the incremented version at the end of the file
+echo "Version: $new_version" >> readme.md
 
 echo "Version updated to $new_version"
