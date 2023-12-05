55a1287cc6386058fe6b1a737cec1ac662acb802
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-05 15:56:58 +0300

    zxc

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Version: 1.10.3
 Version: 1.10.4
+Version: 1.10.4
--- update_version.sh
+++ update_version.sh
@@ -6,7 +6,7 @@ current_version=$(grep -Eo 'Version: [0-9]+\.[0-9]+\.[0-9]+' readme.md | awk '{p
 # Increment the last digit of the version by 1
 new_version=$(echo $current_version | awk -F. '{print $1"."$2"."$3+1}')
 
-# Add a new line with the incremented version below the existing version
-sed -i "/Version: $current_version/a Version: $new_version" readme.md
+# Add a new line with the incremented version after the second line
+sed -i "2 a Version: $new_version" readme.md
 
 echo "Version updated to $new_version"
