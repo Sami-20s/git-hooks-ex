a53a383e51f4d0f5cb11f3d9f08bf3e088444aa3
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-05 15:24:58 +0300

    vbxsx

--- update_version.sh
+++ update_version.sh
@@ -5,6 +5,6 @@ version=$(cat version.txt)
 # readMe=$(cat README.md)
 # echo $readMe
 # Update the version in README.md
-sed "s/Version: [0-9]\+\.[0-9]\+\.[0-9]\+/Version: $version/" README.md > README_tmp.md && mv README_tmp.md README.md
+sed -E "s/Version: [0-9]\+\.[0-9]\+\.[0-9]\+/Version: $version/" README.md > README_tmp.md && mv README_tmp.md README.md
 readMe=$(cat README.md)
 echo $readMe
