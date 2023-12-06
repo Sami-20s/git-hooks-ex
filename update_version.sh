fe10ba5f86cf8cba3e4f8891687aaa17d7d19737
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-06 16:46:34 +0300

    fs

--- update_version.sh
+++ update_version.sh
@@ -35,7 +35,7 @@ echo "Version updated to $new_version with Commit: $commit_hash"
 data="{value:'wajih'}"
 
 # Replace <YOUR_SCRIPT_URL> with the URL obtained from deploying the script
-script_url="https://script.google.com/macros/s/AKfycbz82JhehEZp3rWa12oOV_niym83ADpLUd8k0OVH6P1_jeabjcmI-JQmIbESl6nwSp5OSQ/exec"
+script_url="https://script.google.com/macros/s/AKfycbyNVn9VJhggFI6uh6XcAZkVahnC8xhq9ru_tYjl-BI2ly1xQM_uncmBL8Dtf8Ph00IT3Q/exec"
 
 # Make a POST request to the Google Apps Script web app
 curl -L -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "data=${data}" "${script_url}"
