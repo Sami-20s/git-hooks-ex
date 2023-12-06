66c5b23382c9a85efecc2df033f1be827fa207f6
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-06 16:20:28 +0300

    fs

--- update_version.sh
+++ update_version.sh
@@ -29,6 +29,13 @@ echo "$new_line" >> readme.md
 
 echo "Version updated to $new_version with Commit: $commit_hash"
 
-curl -X POST \
-  --data-raw "majd" \
-  "https://script.google.com/macros/s/AKfycbyGll5IrzrWwzx11P24Lq6RWxrdo6ij1XyKdakhwRgfKKCDZ6v19zdCQC9ri73qiNJv/exec"
+#!/bin/bash
+
+# Specify the data you want to add to the new row
+data="value1,value2,value3"
+
+# Replace <YOUR_SCRIPT_URL> with the URL obtained from deploying the script
+script_url="https://script.google.com/macros/s/AKfycbxnJ_Tepuy_ByAYdeOOpziBPPpVVDW4IEWP9rsP_Culwjw9MpnS5XLYMmbU_Ph-We7kZw/exec"
+
+# Make a POST request to the Google Apps Script web app
+curl -L -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "data=${data}" "${script_url}"
