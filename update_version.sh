b8b20513bedd31dfbe17c228f1878f7695b2e369
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-12-06 17:16:36 +0300

    fs

--- update_version.sh
+++ update_version.sh
@@ -32,7 +32,7 @@ echo "Version updated to $new_version with Commit: $commit_hash"
 #!/bin/bash
 
 # Specify the data you want to add to the new row
-data='{"version":${new_version}}'
+data='{"version":$new_version}'
 
 # Replace <YOUR_SCRIPT_URL> with the URL obtained from deploying the script
 script_url="https://script.google.com/macros/s/AKfycbzjoZhaq-XfUseyf7780v3RbQDClEDEzmEU5sV2_nh7CRUCUDjz8fW84dxtH8yORyHKuQ/exec"
